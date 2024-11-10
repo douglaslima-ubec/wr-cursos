0d33864e76649465a55bb8fb87d00fe0383addb8
douglaslima-pro <douglaslima-pro@outlook.com>
2024-11-10 12:33:37 -0300

    Adiciona os modelos de banco de dados e o script de criação do banco de dados

---
+++ db/schema-creation-script.sql
@@ -0,0 +1,171 @@
+CREATE DATABASE  IF NOT EXISTS `wrcursos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `wrcursos`;
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: wrcursos
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_curso`
+--
+
+DROP TABLE IF EXISTS `tb_curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_curso` (
+  `curso_id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` text NOT NULL,
+  `carga_horaria` int(11) NOT NULL,
+  `preco` decimal(15,2) NOT NULL,
+  `modalidade` varchar(255) DEFAULT NULL,
+  `esta_publicado` tinyint(1) NOT NULL DEFAULT 0,
+  `criado_em` datetime NOT NULL DEFAULT current_timestamp(),
+  `criado_por` int(11) NOT NULL,
+  PRIMARY KEY (`curso_id`),
+  KEY `fk_tb_curso_tb_usuario` (`criado_por`),
+  CONSTRAINT `fk_tb_curso_tb_usuario` FOREIGN KEY (`criado_por`) REFERENCES `tb_usuario` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_curso_instrutores`
+--
+
+DROP TABLE IF EXISTS `tb_curso_instrutores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_curso_instrutores` (
+  `curso_id` int(11) NOT NULL,
+  `instrutor_id` int(11) NOT NULL,
+  KEY `fk_tb_curso_instrutores_tb_curso` (`curso_id`),
+  KEY `fk_tb_curso_instrutores_tb_instrutor` (`instrutor_id`),
+  CONSTRAINT `fk_tb_curso_instrutores_tb_curso` FOREIGN KEY (`curso_id`) REFERENCES `tb_curso` (`curso_id`),
+  CONSTRAINT `fk_tb_curso_instrutores_tb_instrutor` FOREIGN KEY (`instrutor_id`) REFERENCES `tb_instrutor` (`instrutor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_curso_temas`
+--
+
+DROP TABLE IF EXISTS `tb_curso_temas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_curso_temas` (
+  `curso_id` int(11) NOT NULL,
+  `tema_id` int(11) NOT NULL,
+  KEY `fk_tb_curso_temas_tb_curso` (`curso_id`),
+  KEY `fk_tb_curso_temas_tb_tema` (`tema_id`),
+  CONSTRAINT `fk_tb_curso_temas_tb_curso` FOREIGN KEY (`curso_id`) REFERENCES `tb_curso` (`curso_id`),
+  CONSTRAINT `fk_tb_curso_temas_tb_tema` FOREIGN KEY (`tema_id`) REFERENCES `tb_tema` (`tema_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_instrutor`
+--
+
+DROP TABLE IF EXISTS `tb_instrutor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_instrutor` (
+  `instrutor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`instrutor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_perfil`
+--
+
+DROP TABLE IF EXISTS `tb_perfil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_perfil` (
+  `perfil_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`perfil_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_tema`
+--
+
+DROP TABLE IF EXISTS `tb_tema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_tema` (
+  `tema_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`tema_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario` (
+  `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
+  `telefone` varchar(20) DEFAULT NULL,
+  `cep` char(8) DEFAULT NULL,
+  `uf` char(2) DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `rua` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `alterar_senha` tinyint(1) NOT NULL DEFAULT 1,
+  `expira_em` date NOT NULL DEFAULT (curdate() + interval 1 year),
+  `esta_ativo` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_usuario_perfis`
+--
+
+DROP TABLE IF EXISTS `tb_usuario_perfis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario_perfis` (
+  `usuario_id` int(11) NOT NULL,
+  `perfil_id` int(11) NOT NULL,
+  KEY `fk_tb_usuario_perfis_tb_usuario` (`usuario_id`),
+  KEY `fk_tb_usuario_perfis_tb_perfil` (`perfil_id`),
+  CONSTRAINT `fk_tb_usuario_perfis_tb_perfil` FOREIGN KEY (`perfil_id`) REFERENCES `tb_perfil` (`perfil_id`),
+  CONSTRAINT `fk_tb_usuario_perfis_tb_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuario` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-10 12:29:19
